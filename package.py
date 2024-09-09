92978442840b5d8ecfa9097f0dbe5344e54a86bd
robin <robindelillo@gmail.com>
2024-09-09 16:31:54 -0400

    Define app_host_name and ayon_server >= 1.1.2.

--- package.py
+++ package.py
@@ -1,8 +1,10 @@
 name = "hiero"
 title = "Hiero"
 version = "0.2.3-dev.1"
+app_host_name = "hiero"
 client_dir = "ayon_hiero"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
