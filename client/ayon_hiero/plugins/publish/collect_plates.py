e2becd9494e5db9095d34dda472beb168dd2a574
robin@ynput.io <robin@ynput.io>
2024-12-04 16:30:51 -0500

    Rework clip_media option in ayon_core.

--- client/ayon_hiero/plugins/publish/collect_plates.py
+++ client/ayon_hiero/plugins/publish/collect_plates.py
@@ -41,38 +41,6 @@ class CollectPlate(pyblish.api.InstancePlugin):
             if reviewable_source == "clip_media":
                 instance.data["families"].append("review")
                 instance.data.pop("reviewTrack", None)
-
-                # Retrieve source clip media from otio clip.
-                # image sequence
-                if hasattr(otio_clip.media_reference, "target_url_for_image_number"):
-                    file_url = otio_clip.media_reference.target_url_for_image_number(0)
-                    sequence_length = (
-                        otio_clip.media_reference.end_frame()
-                        - otio_clip.media_reference.start_frame
-                    )
-                    file_names = [
-                        os.path.basename(
-                            otio_clip.media_reference.target_url_for_image_number(frame)
-                        )
-                        for frame in range(0, sequence_length)
-                    ]
-
-                # movie
-                else:
-                    file_url = otio_clip.media_reference.target_url
-                    file_names = os.path.basename(file_url)
-
-                _, ext = os.path.splitext(file_url)
-
-                repre = {
-                    "name": ext[1:],
-                    "ext": ext[1:],
-                    "files": file_names,
-                    "stagingDir": os.path.dirname(file_url),
-                    "tags": ["review", "delete"]
-                }
-                instance.data["representations"].append(repre)
-
             else:
                 instance.data["reviewTrack"] = reviewable_source
 
