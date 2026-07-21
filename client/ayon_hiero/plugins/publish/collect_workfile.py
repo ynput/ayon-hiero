2d9c70f89d6cf041120fb001b6f5d6c115ed61bd
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-07-21 16:14:54 +0200

    added collector to add 'otio.timeline.workfile'

---
+++ client/ayon_hiero/plugins/publish/collect_workfile.py
@@ -0,0 +1,14 @@
+import pyblish.api
+
+
+class CollectWorkfile(pyblish.api.InstancePlugin):
+    """Collect additional metadata for workfile instance."""
+
+    label = "Collect Workfile"
+    order = pyblish.api.CollectorOrder - 0.4
+    hosts = ["hiero"]
+    families = ["workfile"]
+
+    def process(self, instance):
+        # Mark instance for 'ExtractOTIOFile' in core
+        instance.data["families"].append("otio.timeline.workfile")
